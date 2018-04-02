99b6b335ef28f13dc6155747b81d01e99944f84c
uotaneet <uotaneet@gmail.com>
2018-04-02 04:13:16 +0000

    create templates

---
+++ README.md
@@ -0,0 +1,4 @@
+# 参考にしたページ
+- [cloud9でflaskアプリを書いてブラウザからアクセスしてみた](https://qiita.com/piruty/items/cbe55ec89a3276475c4a)
+- [Cloud9でpython3を動かす時にやっておきたいこと](https://qiita.com/hahifu/items/6c808a72b1474500acd6)
+- [Flaskのサーバーはデフォルトだと公開されてない](https://qiita.com/tomboyboy/items/122dfdb41188176e45b5)
--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
-def controller():
-    return "controller"
+@app.route('/<username>')
+def controller(username):
+    return render_template('index.html', title='flask test', username=username)
 
 
 if __name__ == "__main__":
